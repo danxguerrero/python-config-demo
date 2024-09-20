f8f6e9c687fd5d0e1987bc13b247cbd6517f19da
Daniel Guerrero <danxguerrero@yahoo.com>
2024-09-20 13:03:33 -0400

    Changed config password value in demo.py

--- demo.py
+++ demo.py
@@ -6,6 +6,11 @@ config.read('config.ini')
 
 username = config.get('settings', 'username')
 password = config.get('settings', 'password')
-debug_mode = config.get('settings', 'debug')
+debug_mode = config.getboolean('settings', 'debug')
 
 print(f'Username: {username}, Password: {password}, Debug Mode: {debug_mode}')
+
+config.set('settings', 'password', 'my_new_password')
+
+with open('config.ini','w') as configfile:
+    config.write(configfile)
